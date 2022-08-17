04cf89a952b8eda6d05171bcd3feaa304aa9d840
krrks <25857593+krrks@users.noreply.github.com>
2022-08-17 19:19:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 def unzipCodeFolder():
     os.system(f"unzip -q -P {os.environ.get('zippsw')} /opt/app-root/src/codes.zip -d /opt/app-root/src/codes")
 if __name__ == "__main__":
-    os.system("git pull")
+    os.system("git config pull.rebase true")
+    os.system("git pull origin master -q 2> /dev/null")
     unzipCodeFolder()
     from codes.main import cron
     while True :
